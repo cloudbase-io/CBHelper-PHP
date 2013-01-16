91cd80a9016b1aba413be20a606ec189cd8b6ac4
Stefano Buliani <stefano@cloudbase.io>
2013-01-16 10:10:55 +0000

    Added doxygen Main page
    Added doxygen Main page

--- CBHelper.php
+++ CBHelper.php
@@ -1,5 +1,31 @@
 <?php
-
+/*! \mainpage cloudbase.io PHP Helper Class Reference
+ *
+ * \section intro_sec Introduction
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 2, as published by
+ * the Free Software Foundation.<br/><br/>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.<br/><br/>
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to the Free
+ * Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.<br/><br/>
+ *
+ * \section install_sec Getting Started
+ *
+ * This class depends on the php Curl extension. Packages are available for most linux distribution
+ * to install through your package manager of choice:<br/><br/>
+ *
+ * This full reference is a companion to <a href="/documentation/php" target="_blank">
+ * the tutorial on the cloudbase.io website<a/>
+ */
+ 
 class CBLogLevel
 {
     const CBLogLevelDebug = "DEBUG";
